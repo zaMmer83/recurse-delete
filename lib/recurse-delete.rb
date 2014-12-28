5de76ebf4666ab18b9c03f408c11db852c1dc543
Simon Kaczor <skaczor@prognosoft.biz>
2014-12-27 19:47:11 -0500

    Added support for polymorphic belongs_to association

--- lib/recurse-delete.rb
+++ lib/recurse-delete.rb
@@ -31,37 +31,50 @@ module RecurseDelete
   end
 
   def delete_recursively(parent_class, parent_ids)
+    # get the assocs for the parent class
+    parent_class.reflect_on_all_associations.select do |assoc|
+      [:destroy, :destroy_all, :delete, :delete_all].include? assoc.options[:dependent]
+    end.each do |assoc|
+      if belongs_to_polymorphic_association?(assoc)
+        foreign_key = assoc.options[:foreign_key] || "#{assoc.name}_id".to_sym
+        foreign_type = assoc.options[:foreign_type] || "#{assoc.name}_type".to_sym
+        parent_class.where(:id => parent_ids).pluck(foreign_type, foreign_key).group_by do |tuple|
+          tuple.first
+        end.each do |dependent_class_name, dependent_ids|
+          delete_recursively(dependent_class_name.constantize, dependent_ids.map(&:second))
+        end
+      else
+        # get the dependent class
+        dependent_class = assoc.name.to_s.classify.constantize
+
+        # get the foreign class; table_name is used to support STI
+        foreign_class = parent_class.table_name.classify
+        if polymorphic_association?(assoc)
+          # get the foreign key
+          foreign_key = assoc.options[:as].to_s.foreign_key
+          # get the foreign type
+          foreign_type = foreign_key.gsub('_id', '_type')
+          # get all the dependent record ids
+          dependent_ids = dependent_class.where(foreign_key => parent_ids, foreign_type => foreign_class).pluck(:id)
+        else
+          foreign_key = assoc.options[:foreign_key].present? ? assoc.options[:foreign_key] : foreign_class.foreign_key
+          dependent_ids = dependent_class.where(foreign_key => parent_ids).pluck(:id)
+        end
+        # recurse
+        delete_recursively(dependent_class, dependent_ids)
+      end
+    end
     # delete all the parent records
     parent_class.delete_all(:id => parent_ids)
 
-    # get the assocs for the parent class
-    assocs = parent_class.reflect_on_all_associations.select do |assoc|
-      [:destroy, :destroy_all, :delete, :delete_all].include? assoc.options[:dependent]
-    end
-    assocs.each do |assoc|
-      # get the dependent class
-      dependent_class = assoc.name.to_s.classify.constantize
-
-      # get the foreign class; table_name is used to support STI
-      foreign_class = parent_class.table_name.classify
-      if polymorphic_association?(assoc)
-        # get the foreign key
-        foreign_key = assoc.options[:as].to_s.foreign_key
-        # get the foreign type
-        foreign_type = foreign_key.gsub('_id', '_type')
-        # get all the dependent record ids 
-        dependent_ids = dependent_class.where(foreign_key => parent_ids, foreign_type => foreign_class).pluck(:id)
-      else
-        foreign_key = assoc.options[:foreign_key].present? ? assoc.options[:foreign_key] : foreign_class.foreign_key
-        dependent_ids = dependent_class.where(foreign_key => parent_ids).pluck(:id)
-      end
-      # recurse
-      delete_recursively(dependent_class, dependent_ids)
-    end
   end
 
   def polymorphic_association?(assoc)
     assoc.options[:as].present?
+  end
+
+  def belongs_to_polymorphic_association?(assoc)
+    assoc.macro == :belongs_to && assoc.options[:polymorphic]
   end
 
   module ClassMethods
